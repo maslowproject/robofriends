be5c51e81bd212cfb81ae694cf3241e4a2bf37bb
Josh A <maslowproject@outlook.com>
2018-12-20 13:04:53 -0600

    circleci cleanup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
-import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import './index.css';
 import App from "./containers/App"
@@ -11,7 +10,6 @@ import 'tachyons'
 import { searchRobots, requestRobots } from './reducers';
 
 const rootReducer = combineReducers({ searchRobots, requestRobots })
-const logger = createLogger()
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
 ReactDOM.render(
